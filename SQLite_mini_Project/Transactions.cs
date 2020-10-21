ff1de04199380328076b2b314e0354c7ebfff4b9
Marnus Tainthong <marnusfrank@gmail.com>
2020-10-21 23:18:26 +0700

    Create Transactions model

---
+++ SQLite_mini_Project/Transactions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLite_mini_Project
+{
+    class Transactions
+    {
+        public static void InitializeDatabase()
+        {
+            using (SqliteConnection db = new SqliteConnection("Filename=sqliteBookStore.db"))
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT EXISTS Transactions (" +
+                    "ISBN INTEGER(13) NULL," +
+                    "Customer_Id INTEGER(5) NULL," +
+                    "Quantity INT(5) NULL," +
+                    "Total_price FLOAT(8) NULL)";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+    }
+}
