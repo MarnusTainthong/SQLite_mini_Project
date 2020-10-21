6a329cb0ea4d59d97c31d3508dc1d7c85b67dfb3
Marnus Tainthong <marnusfrank@gmail.com>
2020-10-21 20:30:08 +0700

    Create Book table

---
+++ SQLite_mini_Project/BookModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLite_mini_Project
+{
+    class BookModel
+    {
+        public static void InitializeDatabase()
+        {
+            using (SqliteConnection db = new SqliteConnection("Filename=sqliteBookStore.db"))
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT EXISTS Books (" +
+                    "ISBN INTEGER(13) PRIMARY KEY," +
+                    "Title NVARCHAR(30) NULL," +
+                    "Description NVARXHAR(30) NULL," +
+                    "Price FLOAT(5) NULL)";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+    }
+}
