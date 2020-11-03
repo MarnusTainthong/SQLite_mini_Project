bd45a4ab520e4a9f8d15c6cda36ce8721763fbfd
Marnus Tainthong <marnusfrank@gmail.com>
2020-11-03 17:35:52 +0700

    ADD book module

--- SQLite_mini_Project/BookModel.cs
+++ SQLite_mini_Project/BookModel.cs
@@ -4,11 +4,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace SQLite_mini_Project
 {
     class BookModel
     {
+        private static string dbpath = "sqliteBookStore.db";
+        private static string tbName = "Books";
+
+        private string bookISBN;
+        private string bookTitle;
+        private string bookDesc;
+        private float bookPrice;
+
+        public string BookISBN { get => bookISBN; set => bookISBN = value; }
+        public string BookTitle { get => bookTitle; set => bookTitle = value; }
+        public string BookDesc { get => bookDesc; set => bookDesc = value; }
+        public float BookPrice { get => bookPrice; set => bookPrice = value; }
+
         public static void InitializeDatabase()
         {
             using (SqliteConnection db = new SqliteConnection("Filename=sqliteBookStore.db"))
@@ -23,6 +37,36 @@ namespace SQLite_mini_Project
 
                 SqliteCommand createTable = new SqliteCommand(tableCommand, db);
                 createTable.ExecuteReader();
+            }
+        }
+
+        public static bool AddBook(string BookISBN, string BookTitle, string BookDesc, float BookPrice)
+        {
+            try
+            {
+                using (SqliteConnection db = new SqliteConnection($"Filename={dbpath}"))
+                {
+                    db.Open();
+
+                    SqliteCommand insertCommand = new SqliteCommand();
+                    insertCommand.Connection = db;
+
+                    insertCommand.CommandText = "INSERT INTO " + tbName + " VALUES (@BookISBN, @BookTitle, @BookDesc, @BookPrice)";
+                    insertCommand.Parameters.AddWithValue("@BookISBN", BookISBN);
+                    insertCommand.Parameters.AddWithValue("@BookTitle", BookTitle);
+                    insertCommand.Parameters.AddWithValue("@BookDesc", BookDesc);
+                    insertCommand.Parameters.AddWithValue("@BookPrice", BookPrice);
+
+                    insertCommand.ExecuteReader();
+
+                    db.Close();
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return false;
             }
         }
     }
--- SQLite_mini_Project/ManageBookForm.xaml.cs
+++ SQLite_mini_Project/ManageBookForm.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,6 +23,52 @@ namespace SQLite_mini_Project
         public ManageBookForm()
         {
             InitializeComponent();
+            BookModel.InitializeDatabase();
+        }
+
+        private void btnAddCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            if (!tbInputISBN.IsReadOnly)
+            {
+                string bookISBN = tbInputISBN.Text;
+                string bookTitle = tbInputBookTitle.Text;
+                string bookDesc = tbInputBookDesc.Text;
+                string bookPrice = tbInputBookPrice.Text;
+
+                if (BookModel.AddBook(bookISBN, bookTitle, bookDesc, float.Parse(bookPrice)))
+                {
+                    //showCustomerData();
+                    MessageBox.Show("เพิ่มข้อมูลสำเร็จ!");
+                }
+                else
+                {
+                    MessageBox.Show("เพิ่มข้อมูลไม่สำเร็จ! โปรดติดต่อเจ้าหน้าที่");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("ไม่สามารถเพิ่มข้อมูลได้ โปรดลองใหม่อีกครั้ง", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            resetFormData();
+        }
+
+        private void tbInputISBN_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
+        }
+
+        private void resetFormData()
+        {
+            tbInputISBN.Clear();
+            tbInputBookTitle.Clear();
+            tbInputBookDesc.Clear();
+            tbInputBookPrice.Clear();
+        }
+
+        private void tbInputBookPrice_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = Regex.IsMatch(e.Text, "[^0-9.]+");
         }
     }
 }
